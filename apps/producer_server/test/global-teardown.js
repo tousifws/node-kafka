39bfff8ebfb00a1a1a5bb1b9fa6ecd3cd3ab800c
tousifws <82563595+tousifws@users.noreply.github.com>
2022-03-15 12:43:37 +0530

    chore: run coverage summary

--- apps/producer_server/test/global-teardown.js
+++ apps/producer_server/test/global-teardown.js
@@ -4,8 +4,6 @@ const { down } = require("docker-compose");
 module.exports = async () => {
     // Check if running CI environment
     if (isCI) {
-        console.log("****************");
-        console.log(isCI);
         await down({ commandOptions: ["--remove-orphans"] });
     }
 };
